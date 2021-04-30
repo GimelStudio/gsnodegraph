8da88a6aea39e2698ddb2b53935575124e9d8b86
Noah Rahm <correctsyntax@yahoo.com>
2021-04-30 16:06:48 -0500

    fixes to setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
   license='Apache 2.0',   
   description = 'Powerful nodegraph widget for wxpython GUIs',
   long_description_content_type="text/markdown",
-  author = 'Correct Syntax, Noah Rahm', 
+  author = 'Noah Rahm', 
   author_email = 'correctsyntax@yahoo.com',     
-  url = 'https://github.com/Correct-Syntax/gsnodegraph', 
+  url = 'https://github.com/GimelStudio/gsnodegraph', 
   keywords = ['nodegraph', 'nodes', 'graph', 'node-based'], 
   install_requires=[           
           'wxpython==4.1.1'
