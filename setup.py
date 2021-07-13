10e27a623a742d38eff64ecff2a2d7dac2d9390f
Noah Rahm <correctsyntax@yahoo.com>
2021-07-13 17:07:54 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup
 
 
 setup(
-  name = 'gsnodegraph',   
-  packages = ['gsnodegraph', 
-              'gsnodegraph.graph', 
-              'gsnodegraph.graph.utils', 
-              'gsnodegraph.node'],   
-  version = '0.1.9',
-  license='Apache 2.0',   
+  name = 'gsnodegraph',
+  packages = ['gsnodegraph',
+              'gsnodegraph.graph',
+              'gsnodegraph.graph.utils',
+              'gsnodegraph.node'],
+  version = '0.2.0',
+  license='Apache 2.0',
   description = 'Powerful nodegraph widget for wxpython GUIs',
   long_description_content_type="text/markdown",
-  author = 'Noah Rahm', 
-  author_email = 'correctsyntax@yahoo.com',     
-  url = 'https://github.com/GimelStudio/gsnodegraph', 
-  keywords = ['nodegraph', 'nodes', 'graph', 'node-based'], 
-  install_requires=[           
+  author = 'Noah Rahm',
+  author_email = 'correctsyntax@yahoo.com',
+  url = 'https://github.com/GimelStudio/gsnodegraph',
+  keywords = ['nodegraph', 'nodes', 'graph', 'node-based'],
+  install_requires=[
           'wxpython==4.1.1'
       ],
   classifiers=[
@@ -24,7 +24,7 @@ setup(
     'Operating System :: OS Independent',
     'Topic :: Desktop Environment',
     'Topic :: Multimedia :: Graphics :: Editors',
-    'License :: OSI Approved :: Apache Software License', 
+    'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
