c21cdba16fa3878580f451fb0ccbf5e54daae26f
Noah Rahm <correctsyntax@yahoo.com>
2021-11-19 16:24:15 -0600

    Add a bitmap for Add node button

--- gsnodegraph/assets/bitmaps.py
+++ gsnodegraph/assets/bitmaps.py
@@ -1,5 +1,28 @@
 from wx.lib.embeddedimage import PyEmbeddedImage
 #--------------------
+ICON_ADD_NODE = PyEmbeddedImage(
+    b'iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAABHNCSVQICAgIfAhkiAAAA9ZJ'
+    b'REFUWIXtWUF24jgQ/WVpP7lBe04w0cPs6ROEPkHICZqcoDMnSPcJEk7Q5ASh14gncoIxNyB7'
+    b'yzULZKIHklsmkJkFf0MsS/rfJVVJVQHOOOOM/xx0jEmMMRfW2isiygGAmQcAQEQz91wKIZ6U'
+    b'Uutj8B0s2gm9BjAEMEgcNgMwFUJM3vMBB4nWWl8D+A7gIvD6lZmXAEBElwD+CPQpAdwVRTE5'
+    b'hL+TaGPMZVVVD05MgxcAMyHEo1JqGRtnrR1hsyJ/Ne3MvJRSflFKlScRPZ/PB0T0E2/WXQEY'
+    b'F0Ux7UKotR5is0qfXNOamb/0+/1Z6hxJoheLxYiZH7aDiG57vd73LmIDc46Z+d6b86bX6z2m'
+    b'jP2taLe0z9hY+BXAqKt1Y3BWf8Rm36+FEJ9jW8xHq2gXIf5xgiGEUCmTdoEzinGPpeNojSxZ'
+    b'20tr7TZCENFNqmCt9Vet9deUvkqpJRHduMfccbYiaukdC0yKohiliHAO+wwAzPw51cG01lMA'
+    b'VwAghPizLaJELW2t3TqJEOIuhfg9EEKMPe6Htr5B0caYHO6UY+a/u8bRQ+A4frjHgTEmdHAB'
+    b'iIiu63p7LEspjxIpUiCE2Ia8uq6HsX5B0czcDFgdO1q0wXGtdjTsIbanr9zvh1nZQ8N5FevQ'
+    b'GvKY+ShXyS5I4ZS7DcaY3FoLACCi4NaYz+f3O5cmn9R3oHutdVAEMy/7/f7tbrvPaYzJQ0Fg'
+    b'T3RVVTkRNRMHCYloHGp37/y/gx/m3g0A7Ilm5nUzR1VVOTbX2HbRUsrSs3Qs7Pxg5qAgN6a5'
+    b'fr60fHhwFX1OKWUw1O6JVkqVWmsAgBO254xFUUQtvXMijrtcORvOxtKx86HVEVssfTJ4nK+x'
+    b'PjHRT+43GnZOiIYzukJB0UTUbIncGBN1pmPDceU7GvYQFJ1l2fYrq6qKnkzHhrV26ytZlnUT'
+    b'rZQqmfkXABDRN3eBOikcxzUAMPOvtkQg6ohSyu392b+mngo+h88dQlS0CzdNXWK4WCySkoB+'
+    b'vz8jolsiuk0Nd27uZhtOfncVTskRS7iCywfkiCshxOW7ckSl1FoIMYCLmdbaZ5dBHwVa66HL'
+    b'9AHgVQgxTCmXtYoGtoln49UXAH6mJq1tWCwW3wD4xZ9R6ip2rTBN8VabKwHcHlhhuoeLx9hY'
+    b'eNBl23Wt5eXW2im8ehyAkpmnUspJWy2vqqprIhp6YgHgxW2J09TyfLgy2R3e6nE+1gAa8ZcI'
+    b'V1ZXRHSXWgbbxbvq03Vdj1wBPfWO8kREsyzLHj+8Pr0L9wHDuq6bpW+y+RkAZFlWZlk2PdZ/'
+    b'As4444z/Af4FELLNYCJ7/vwAAAAASUVORK5CYII=')
+
+#----------------------------------------------------------------------
 ICON_BRUSH_CHECKERBOARD = PyEmbeddedImage(
     b'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAABhGlDQ1BJQ0MgcHJvZmlsZQAA'
     b'KJF9kT1Iw0AcxV9TRdGqqB1EHDJUJwuiIo5SxSJYKG2FVh1MLv2CJg1Jiouj4Fpw8GOx6uDi'
