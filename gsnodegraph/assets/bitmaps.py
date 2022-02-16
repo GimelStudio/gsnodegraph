d9af1ac992ce0de31622ea8ce59e1b06fb0ef39d
Noah Rahm <correctsyntax@yahoo.com>
2022-02-16 11:23:13 -0600

    Fix checkerboard img colors

--- gsnodegraph/assets/bitmaps.py
+++ gsnodegraph/assets/bitmaps.py
@@ -24,57 +24,8 @@ ICON_ADD_NODE = PyEmbeddedImage(
 
 #----------------------------------------------------------------------
 ICON_BRUSH_CHECKERBOARD = PyEmbeddedImage(
-    b'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAAABhWlDQ1BJQ0MgcHJvZmlsZQAA'
-    b'KJF9kT1Iw0AcxV9TRZFKETuIOGSoThakigguUsUiWChthVYdTC79giYNSYqLo+BacPBjserg'
-    b'4qyrg6sgCH6AODo5KbpIif9LCi1iPDjux7t7j7t3gNCoMNXsmgBUzTJS8ZiYza2KPa8QEMQA'
-    b'ZhGVmKkn0osZeI6ve/j4ehfhWd7n/hz9St5kgE8knmO6YRFvEE9vWjrnfeIQK0kK8TnxuEEX'
-    b'JH7kuuzyG+eiwwLPDBmZ1DxxiFgsdrDcwaxkqMRTxGFF1ShfyLqscN7irFZqrHVP/sJAXltJ'
-    b'c53mCOJYQgJJiJBRQxkVWIjQqpFiIkX7MQ//sONPkksmVxmMHAuoQoXk+MH/4He3ZmEy6iYF'
-    b'YkD3i21/jAI9u0Czbtvfx7bdPAH8z8CV1vZXG8DMJ+n1thY+AoLbwMV1W5P3gMsdYOhJlwzJ'
-    b'kfw0hUIBeD+jb8oBg7dA35rbW2sfpw9AhrpavgEODoGxImWve7y7t7O3f8+0+vsB9DVy2xw+'
-    b'I+UAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfmAg8SAxV3ONE/AAAAGXRFWHRDb21t'
-    b'ZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAOZJREFUeNrt1qENhAAQBED4UAAC8R3gcN9/'
-    b'Oy8QlEAFkDVkBbP2cmZyl+y4br/hOt9lvpn+9+NVu59B4sCCBQsWLFgCC9YzmXT0fNdleUNY'
-    b'sGDBEliwYPUbvI6e77osbwgLFixYAgsWrH6D19HzXZflDWHBggVLYMGC1W/wOnq+67K8ISxY'
-    b'sGAJLFiw+g1eR893XZY3hAULFiyBBQtWv8Hr6Pmuy/KGsGDBgiWwYMHqN3gdPd91Wd4QFixY'
-    b'sAQWLFj9Bq+j57suyxvCggULlsCCBavf4HX0fNdleUNYsGDBEliwYNVzAtHclKDeAQarAAAA'
-    b'AElFTkSuQmCC')
-
-#----------------------------------------------------------------------
-ICON_IMAGE = PyEmbeddedImage(
-    b'iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAACXBIWXMAAA7DAAAOwwHHb6hk'
-    b'AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAHBJREFUKJHVkksOgCAM'
-    b'RJ/GExDLYUw8vXga4Azjyg0QDcGNs2z6+pkWBjQBpJROSVsHF8xsByDGqJ6Od/7cA5VaGlUz'
-    b'4Mq4pOy9Xx9hwJnZ1CharTY09k/hyjBJuWWOpPwKl+d40idjh84XPUaaAnAB0HEs5elgUi0A'
-    b'AAAASUVORK5CYII=')
-
-#----------------------------------------------------------------------
-ICON_ADD_NODE = PyEmbeddedImage(
-    b'iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAABHNCSVQICAgIfAhkiAAAA9ZJ'
-    b'REFUWIXtWUF24jgQ/WVpP7lBe04w0cPs6ROEPkHICZqcoDMnSPcJEk7Q5ASh14gncoIxNyB7'
-    b'yzULZKIHklsmkJkFf0MsS/rfJVVJVQHOOOOM/xx0jEmMMRfW2isiygGAmQcAQEQz91wKIZ6U'
-    b'Uutj8B0s2gm9BjAEMEgcNgMwFUJM3vMBB4nWWl8D+A7gIvD6lZmXAEBElwD+CPQpAdwVRTE5'
-    b'hL+TaGPMZVVVD05MgxcAMyHEo1JqGRtnrR1hsyJ/Ne3MvJRSflFKlScRPZ/PB0T0E2/WXQEY'
-    b'F0Ux7UKotR5is0qfXNOamb/0+/1Z6hxJoheLxYiZH7aDiG57vd73LmIDc46Z+d6b86bX6z2m'
-    b'jP2taLe0z9hY+BXAqKt1Y3BWf8Rm36+FEJ9jW8xHq2gXIf5xgiGEUCmTdoEzinGPpeNojSxZ'
-    b'20tr7TZCENFNqmCt9Vet9deUvkqpJRHduMfccbYiaukdC0yKohiliHAO+wwAzPw51cG01lMA'
-    b'VwAghPizLaJELW2t3TqJEOIuhfg9EEKMPe6Htr5B0caYHO6UY+a/u8bRQ+A4frjHgTEmdHAB'
-    b'iIiu63p7LEspjxIpUiCE2Ia8uq6HsX5B0czcDFgdO1q0wXGtdjTsIbanr9zvh1nZQ8N5FevQ'
-    b'GvKY+ShXyS5I4ZS7DcaY3FoLACCi4NaYz+f3O5cmn9R3oHutdVAEMy/7/f7tbrvPaYzJQ0Fg'
-    b'T3RVVTkRNRMHCYloHGp37/y/gx/m3g0A7Ilm5nUzR1VVOTbX2HbRUsrSs3Qs7Pxg5qAgN6a5'
-    b'fr60fHhwFX1OKWUw1O6JVkqVWmsAgBO254xFUUQtvXMijrtcORvOxtKx86HVEVssfTJ4nK+x'
-    b'PjHRT+43GnZOiIYzukJB0UTUbIncGBN1pmPDceU7GvYQFJ1l2fYrq6qKnkzHhrV26ytZlnUT'
-    b'rZQqmfkXABDRN3eBOikcxzUAMPOvtkQg6ohSyu392b+mngo+h88dQlS0CzdNXWK4WCySkoB+'
-    b'vz8jolsiuk0Nd27uZhtOfncVTskRS7iCywfkiCshxOW7ckSl1FoIMYCLmdbaZ5dBHwVa66HL'
-    b'9AHgVQgxTCmXtYoGtoln49UXAH6mJq1tWCwW3wD4xZ9R6ip2rTBN8VabKwHcHlhhuoeLx9hY'
-    b'eNBl23Wt5eXW2im8ehyAkpmnUspJWy2vqqprIhp6YgHgxW2J09TyfLgy2R3e6nE+1gAa8ZcI'
-    b'V1ZXRHSXWgbbxbvq03Vdj1wBPfWO8kREsyzLHj+8Pr0L9wHDuq6bpW+y+RkAZFlWZlk2PdZ/'
-    b'As4444z/Af4FELLNYCJ7/vwAAAAASUVORK5CYII=')
-
-#----------------------------------------------------------------------
-ICON_BRUSH_CHECKERBOARD = PyEmbeddedImage(
-    b'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEU/QkUnLjW+BDL7AAAA'
-    b'KElEQVQ4y2P4f4D/D4xgGGAeg/0H5v8wYmB5o+EyGi6j4TIaLrTiAQDr6oSnZbt/HwAAAABJ'
+    b'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkAQMAAABKLAcXAAAABlBMVEVfYWQnLjUoH6AuAAAA'
+    b'KElEQVQ4y2NgsP/A/B9GDDDv/wH+PzCCYWB5o+EyGi6j4TIaLrTiAQDYaoSnfkBkDwAAAABJ'
     b'RU5ErkJggg==')
 
 #----------------------------------------------------------------------
